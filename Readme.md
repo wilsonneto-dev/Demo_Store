4f0155a310758d693c952f5ca5ca0174821e41f8
Wilson Neto <contato@wilsonneto.com.br>
2024-05-25 22:20:52 -0300

    build: Add build images sh script

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Demo Store
+
+To build the services images and publish: \
+` ./build-images.sh`
+
+## Catalog Service
+
+- Container image: https://hub.docker.com/repository/docker/wilsonnetodev/demo-store-catalog/general
+
---
+++ build-images.sh
@@ -0,0 +1,7 @@
+version=v1
+
+docker build . --file ./src/Services/Catalog/Catalog.APi/Dockerfile -t wilsonnetodev/demo-store-catalog:$version
+docker tag wilsonnetodev/demo-store-catalog:$version wilsonnetodev/demo-store-catalog:latest
+
+docker push wilsonnetodev/demo-store-catalog:$version
+docker push wilsonnetodev/demo-store-catalog:latest
